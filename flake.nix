MOIS3Y/nix

add: NvChad 2.5 nix flake with package overlay and home-manager module

flake.nix
@@ -1,0 +1,50 @@
+{
+  description = ''
+    NvChad is Blazing fast Neovim config
+    providing solid defaults and a beautiful UI https://nvchad.com/
+    This home manager module will add NvChad configuration to your Nix setup
+    You can specify in the configuration your own extended configuration
+    built on the starter repository
+    You can also add runtime dependencies that will be isolated from the main
+    system but available to NvChad. This is useful for adding lsp servers.
+    If you are using your own Neovim build and not from nixpkgs
+    you can also specify your package.
+    In addition, you can continue to configure NvChad in the usual way
+    manually by disabling the hm-activation option
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+in {
+  # Executed by `nix build .#<name>`
+  packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+  in rec {
+    nvchad = pkgs.callPackage ./nix/nvchad.nix {};
+    default = nvchad;
+  });
+  # Executed by `nix run .#<name>
+  apps = forAllSystems (system: rec {
+    nvchad = {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/nvchad";
+    };
+    default = nvchad;
+  });
+  overlays = (import ./nix/overlays.nix { })
+  // { default = self.overlays.nvchad ; };
+  homeManagerModules.nvchad = import ./nix/module.nix;
+  homeManagerModule = self.homeManagerModules.nvchad;
+  };
+}

nix/nvchad.nix
@@ -1,0 +1,68 @@
+# █▄░█ █░█ █▀▀ █░█ ▄▀█ █▀▄ ▀
+# █░▀█ ▀▄▀ █▄▄ █▀█ █▀█ █▄▀ ▄
+# -- -- -- -- -- -- -- -- --
+
+{ stdenvNoCC
+,fetchFromGitHub
+,makeWrapper
+,lib
+,coreutils
+,findutils
+,git
+,gcc
+,neovim
+,nodejs
+,lua5_1
+,lua-language-server
+,ripgrep
+,tree-sitter
+,extraPackages ? []
+,extraConfig ? ./starter.nix
+}: with lib;
+stdenvNoCC.mkDerivation rec {
+  pname = "nvchad";
+  version = "2.5";
+  src = (
+    if extraConfig == ./starter.nix then fetchFromGitHub (import extraConfig) 
+    else extraConfig
+  );
+  nvChadBin = ../bin/nvchad.sh;
+  nvChadContrib = ../contrib;
+  buildInputs = [ makeWrapper ];
+  nativeBuildInputs = (
+    lists.unique (
+      extraPackages ++ [
+        coreutils
+        findutils
+        git
+        gcc
+        nodejs
+        lua-language-server
+        (lua5_1.withPackages(ps: with ps; [ luarocks ]))
+        ripgrep
+        tree-sitter
+      ]
+    )
+  ) ++ [ neovim ];
+  installPhase = ''
+    mkdir -p $out/{bin,config}
+    cp -r $src/* $out/config
+    install -Dm755 $nvChadBin $out/bin/nvchad
+    wrapProgram $out/bin/nvchad --prefix PATH : '${makeBinPath nativeBuildInputs}'
+    runHook postInstall
+  '';
+  postInstall = ''
+    mkdir -p $out/share/{applications,icons/hicolor/scalable/apps}
+    cp $nvChadContrib/NvChad.desktop $out/share/applications
+    cp $nvChadContrib/nvchad.svg $out/share/icons/hicolor/scalable/apps
+  '';
+  meta = {
+    description = ''
+      Blazing fast Neovim config providing solid defaults and a beautiful UI
+    '';
+    homepage = "https://nvchad.com/";
+    license = licenses.gpl3;
+    mainProgram = "nvchad";
+    maintainers = with maintainers; [ MOIS3Y ];
+  };
+}

nix-community/nix4nvchad

flake.nix
@@ -1,0 +1,53 @@
+{
+  description = ''
+    NvChad is Blazing fast Neovim config
+    providing solid defaults and a beautiful UI https://nvchad.com/
+    This home manager module will add NvChad configuration to your Nix setup
+    You can specify in the configuration your own extended configuration
+    built on the starter repository
+    You can also add runtime dependencies that will be isolated from the main
+    system but available to NvChad. This is useful for adding lsp servers.
+    If you are using your own Neovim build and not from nixpkgs
+    you can also specify your package.
+    In addition, you can continue to configure NvChad in the usual way
+    manually by disabling the hm-activation option
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+in {
+  # Executed by `nix build .#<name>`
+  packages = forAllSystems (system: let
+    pkgs = nixpkgsFor.${system};
+  in rec {
+    nvchad = pkgs.callPackage ./nix/nvchad.nix {};
+    default = nvchad;
+  });
+  # Executed by `nix run .#<name>
+  apps = forAllSystems (system: rec {
+    nvchad = {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/nvim";
+    };
+    default = nvchad;
+  });
+  overlays = (import ./nix/overlays.nix { })
+  // { default = self.overlays.nvchad ; };
+  homeManagerModules = rec {
+    nvchad = import ./nix/module.nix;
+    default = nvchad;
+  };
+  homeManagerModule = self.homeManagerModules.nvchad;
+  };
+}

nix/nvchad.nix
@@ -1,0 +1,68 @@
+# █▄░█ █░█ █▀▀ █░█ ▄▀█ █▀▄ ▀
+# █░▀█ ▀▄▀ █▄▄ █▀█ █▀█ █▄▀ ▄
+# -- -- -- -- -- -- -- -- --
+
+{ stdenvNoCC
+,fetchFromGitHub
+,makeWrapper
+,lib
+,coreutils
+,findutils
+,git
+,gcc
+,neovim
+,nodejs
+,lua5_1
+,lua-language-server
+,ripgrep
+,tree-sitter
+,extraPackages ? []
+,extraConfig ? ./starter.nix
+}: with lib;
+stdenvNoCC.mkDerivation rec {
+  pname = "nvchad";
+  version = "2.5";
+  src = (
+    if extraConfig == ./starter.nix then fetchFromGitHub (import extraConfig) 
+    else extraConfig
+  );
+  nvChadBin = ../bin/nvchad.sh;
+  nvChadContrib = ../contrib;
+  buildInputs = [ makeWrapper ];
+  nativeBuildInputs = (
+    lists.unique (
+      extraPackages ++ [
+        coreutils
+        findutils
+        git
+        gcc
+        nodejs
+        lua-language-server
+        (lua5_1.withPackages(ps: with ps; [ luarocks ]))
+        ripgrep
+        tree-sitter
+      ]
+    )
+  ) ++ [ neovim ];
+  installPhase = ''
+    mkdir -p $out/{bin,config}
+    cp -r $src/* $out/config
+    install -Dm755 $nvChadBin $out/bin/nvim
+    wrapProgram $out/bin/nvim --prefix PATH : '${makeBinPath nativeBuildInputs}'
+    runHook postInstall
+  '';
+  postInstall = ''
+    mkdir -p $out/share/{applications,icons/hicolor/scalable/apps}
+    cp $nvChadContrib/nvim.desktop $out/share/applications
+    cp $nvChadContrib/nvchad.svg $out/share/icons/hicolor/scalable/apps
+  '';
+  meta = {
+    description = ''
+      Blazing fast Neovim config providing solid defaults and a beautiful UI
+    '';
+    homepage = "https://nvchad.com/";
+    license = licenses.gpl3;
+    mainProgram = "nvim";
+    maintainers = with maintainers; [ MOIS3Y ];
+  };
+}